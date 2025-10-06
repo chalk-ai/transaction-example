67e9b2290f71f1b249a92e64d66fb6bd0bc83aaa
elvis kahoro <github@elvis.ai>
2025-10-06 14:13:59 -0700

    fix: changing to scout cause of a change in the tool call

--- src/groq.py
+++ src/groq.py
@@ -2,5 +2,5 @@ import os
 
 GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", None)
 GROQ_MODEL_PROVIDER: str = "openai"
-GROQ_MODEL: str = "openai/gpt-oss-20b"
+GROQ_MODEL: str = "meta-llama/llama-4-scout-17b-16e-instruct"
 GROQ_BASE_URL: str = "https://api.groq.com/openai/v1"
