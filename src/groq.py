42395b3cee8f0a4ba8037e56a5a9fff56e6d8725
elvis kahoro <github@elvis.ai>
2025-10-06 10:48:52 -0700

    chore: updating groq model

--- src/groq.py
+++ src/groq.py
@@ -1,8 +1,6 @@
-from chalk.features import before_all
 import os
-from chalk import chalk_logger
 
 GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", None)
 GROQ_MODEL_PROVIDER: str = "openai"
-GROQ_MODEL: str = "llama3-8b-8192"
+GROQ_MODEL: str = "openai/gpt-oss-20b"
 GROQ_BASE_URL: str = "https://api.groq.com/openai/v1"
