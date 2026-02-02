6d35e51bb481b322b4121f49371ee90e2ffabe95
Elliot Marx <elliot@chalk.ai>
2026-02-02 15:33:21 -0800

    fix import

--- src/model_registry.py
+++ src/model_registry.py
@@ -1,4 +1,4 @@
-from models import User
+from src.models import User
 from chalk import make_model_resolver
 from chalk.ml import ModelReference
 
@@ -7,7 +7,7 @@ fraud_detection_model = ModelReference.from_version(
     name="fraud_detection_model", version=1
 )
 
-_ = make_model_resolver(
+make_model_resolver(
     name="fraud_detection_model",
     model=fraud_detection_model,
     input=[
@@ -16,8 +16,8 @@ _ = make_model_resolver(
         User.domain_age_days,
         User.name_email_match_score,
         User.credit_report.percent_past_due,
-        User.count_withdrawals__86400__,
-        User.count_withdrawals__604800__,
+        User.count_withdrawals["1d"],
+        User.count_withdrawals["7d"],
         User.total_spend,
     ],
     output=User.is_fraud,
